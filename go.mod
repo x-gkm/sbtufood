83b3a54f37eff8fb759a0e669e1a124d884fc4d1
Gökmen Kaplan <48455693+x-gkm@users.noreply.github.com>
2026-01-04 17:41:45 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/x-gkm/sbtufood
+
+go 1.25.5
+
+require (
+	github.com/PuerkitoBio/goquery v1.11.0
+	github.com/goodsign/monday v1.0.2
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	golang.org/x/net v0.47.0 // indirect
+)
---
+++ menu/menu.go
@@ -0,0 +1,99 @@
+package menu
+
+import (
+	"errors"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+	"unicode"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/goodsign/monday"
+)
+
+var NotFoundError = errors.New("couldn't find today's menu")
+var NoServiceError = errors.New("food service is not available during weekends")
+
+var locationTr *time.Location
+
+func init() {
+	var err error
+	locationTr, err = time.LoadLocation("Europe/Istanbul")
+	if err != nil {
+		panic(err)
+	}
+}
+
+type Menu struct {
+	Date  time.Time
+	Items []string
+}
+
+func extract(reader io.Reader) ([]Menu, error) {
+	doc, err := goquery.NewDocumentFromReader(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	var menus []Menu
+	for _, s := range doc.Find(".yemekhane-primary").EachIter() {
+		title := s.Find(".yemekhane-title").Text()
+		date, err := monday.ParseInLocation("Monday 02 January 2006 15:04", title, locationTr, monday.LocaleTrTR)
+		if err != nil {
+			return nil, err
+		}
+
+		var items []string
+		for _, s := range s.Find("li").EachIter() {
+			raw := s.Contents().Last().Text()
+			for item := range strings.SplitSeq(raw, "/") {
+				item = strings.TrimSpace(item)
+				item = strings.ToLowerSpecial(unicode.TurkishCase, item)
+				items = append(items, item)
+			}
+		}
+
+		menus = append(menus, Menu{date, items})
+	}
+
+	return menus, nil
+}
+
+func Month() ([]Menu, error) {
+	resp, err := http.Get("https://sivas.edu.tr/yemek-listesi")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return extract(resp.Body)
+}
+
+func dateEqual(a, b time.Time) bool {
+	ya, ma, da := a.Date()
+	yb, mb, db := b.Date()
+	return ya == yb && ma == mb && da == db
+}
+
+func Today() (Menu, error) {
+	now := time.Now().In(locationTr)
+
+	switch now.Weekday() {
+	case time.Saturday, time.Sunday:
+		return Menu{}, NoServiceError
+	}
+
+	month, err := Month()
+	if err != nil {
+		return Menu{}, err
+	}
+
+	for _, day := range month {
+		if dateEqual(day.Date, now) {
+			return day, nil
+		}
+	}
+
+	return Menu{}, NotFoundError
+}
